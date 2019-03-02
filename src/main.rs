c6eece868d88d7513fda6cb3aa16b1fea19ebc9b
Joshua Maddux <josh@pkcsecurity.com>
2019-03-01 17:57:26 -0800

    add rate limiting

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,32 @@ use std::collections::HashMap;
 use spylent::models::*;
 use spylent::connection;
 use diesel;
-use spylent::schema::*;
+use spylent::schema::points::dsl::*;
+use diesel::sql_types::*;
+use diesel::dsl::sql;
 use spylent::connection::*;
 use diesel::query_dsl::*;
 use diesel::QueryResult;
 use dotenv::dotenv;
 use reqwest;
 
-//fn threshold() {
-//    dotenv("THRESHOLD").map(|t| t.parse() as i64).or_else(1)
-//}
-
 #[post("/api/point", format = "application/json", data = "<point>")]
 fn add_point(point: Json<NewPoint>, connection: DbConn) -> String {
     let slack_url = dotenv::var("SLACK_URL").expect("No slack url configured :(");
     let threshold: f64 = dotenv::var("THRESHOLD").expect("No threshold configured :(").parse().unwrap();
-    if &point.value > &threshold {
+    let query = sql::<BigInt>("SELECT count(*) FROM points WHERE created > extract(epoch from now()) - 60000 AND value > 10.0;");
+    let last_n = *query.load::<i64>(&*connection).expect("Can't query points").first().unwrap();
+    println!("{:?}", last_n);
+    if &point.value > &threshold && last_n < 1 {
         let mut map = HashMap::new();
         map.insert("text", "COFFEE");
         let client = reqwest::Client::new();
-        let res = client.post(&slack_url)
+        let _res = client.post(&slack_url)
             .json(&map)
             .send();
     }
     let new_point: NewPoint = point.into_inner();
-    let _: QueryResult<Point> = diesel::insert_into(points::table)
+    let _: QueryResult<Point> = diesel::insert_into(points)
         .values(&new_point)
         .get_result(&*connection);
     "Success".into()
